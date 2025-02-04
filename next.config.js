9075228951b0b691ac1bc743830b83350606de6b
HelplineCl <girlsandthediamonds@gmail.com>
2025-02-04 18:23:20 -0300

    Setup deploy to GitHub Pages

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  exportTrailingSlash: true,
   output: 'export',
   basePath: process.env.NODE_ENV === 'production' ? '' : undefined,
   experimental: {
