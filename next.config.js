ca1978fe8b40da9c7974f5101f317c254030bfbc
HelplineCl <girlsandthediamonds@gmail.com>
2025-02-04 18:51:43 -0300

    base url

--- next.config.js
+++ next.config.js
@@ -1,14 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  basePath: 'https://helplinecl.github.io/website/',
   exportTrailingSlash: true,
   output: 'export',
   basePath: process.env.NODE_ENV === 'production' ? '' : undefined,
-  experimental: {
-    appDir: true,
-  },
   images: {
     unoptimized: true,
+  },
+  experimental: {
+    exportTrailingSlash: true,
   },
   trailingSlash: true,
   reactStrictMode: true,
