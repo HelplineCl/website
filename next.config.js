87903d1dd5a60013e43b7575ee2f849a6bc21716
HelplineCl <girlsandthediamonds@gmail.com>
2025-02-04 18:17:08 -0300

    package.json

--- next.config.js
+++ next.config.js
@@ -2,6 +2,14 @@
 const nextConfig = {
   reactStrictMode: true,
   output: 'export',
+  basePath: process.env.NODE_ENV === 'production' ? '' : undefined,
+  experimental: {
+    appDir: true,
+  },
+  images: {
+    unoptimized: true,
+  },
+  reactStrictMode: true,
 }
 
 module.exports = nextConfig
